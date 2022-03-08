acfc58660a197e6d18a3dfda1e617358dfbc3bfa
kryshe4ka <kryshe4ka@gmail.com>
2022-03-08 20:41:04 +0400

    feat: set up firebase & email-password log in

---
+++ Messenger/Controllers/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Messenger
+//
+//  Created by Liza Kryshkovskaya on 21.02.22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
